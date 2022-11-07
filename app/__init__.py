b1b445c355c21ff1b8e66a89bc859fc9f20d67fa
David F Mueth <78443941+DoctorMuch@users.noreply.github.com>
2022-11-07 14:15:02 -0600

    set up virtual environment and routes

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from app.routes import home, dashboard
+
+from flask import Flask
+
+def create_app(test_config=None):
+
+  app = Flask(__name__, static_url_path='/')
+  app.url_map.strict_slashes = False
+  app.config.from_mapping(
+    SECRET_KEY='whirl_twirl_squirrel'
+  )
+
+  @app.route('/hello')
+  def hello():
+    return 'hello world'
+
+  app.register_blueprint(home)
+  app.register_blueprint(dashboard)
+
+  return app
